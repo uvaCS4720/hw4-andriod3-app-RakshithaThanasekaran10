49ceab9294c075df8dd64a322e8cc5be2c8a9e03
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:08:08 -0400

    commented PlaceEntity.kt

--- app/src/main/java/edu/nd/pmcburne/hello/data/PlaceEntity.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/data/PlaceEntity.kt
@@ -3,6 +3,8 @@ package edu.nd.pmcburne.hello.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+//represents a table in the Room database
+// each instance of placeEntity corresponds to a row
 @Entity
 data class PlaceEntity(
     @PrimaryKey val id: Int,
