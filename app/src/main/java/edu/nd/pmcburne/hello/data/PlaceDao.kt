fe810695512fd162853d2623588dbf8b1ac15909
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:07:59 -0400

    commented placedaokt

--- app/src/main/java/edu/nd/pmcburne/hello/data/PlaceDao.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/data/PlaceDao.kt
@@ -6,11 +6,15 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+//marks this interface as a DAO for rOOM
+//DAOs define how the app interacts with the database
 @Dao
 interface PlaceDao {
+    //SQL query to retrieve all rows from the table
     @Query("SELECT * FROM placeentity")
     fun getAllPlacesFlow(): Flow<List<PlaceEntity>>
 
+    //Inserts a new PlaceEntity into the database
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertOrIgnore(place: PlaceEntity)
 }
