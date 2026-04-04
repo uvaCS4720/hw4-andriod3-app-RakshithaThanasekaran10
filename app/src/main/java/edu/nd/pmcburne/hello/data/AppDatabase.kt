6aff3bf41b74dc686a830ce752edef5ec85ee595
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:07:05 -0400

    commented appdatabase.kt

--- app/src/main/java/edu/nd/pmcburne/hello/data/AppDatabase.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/data/AppDatabase.kt
@@ -5,25 +5,34 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+// Defines room databse with one entity
 @Database(entities = [PlaceEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
+    //abstract funtion that provides access to the DAO
+    //Room generates the implementation at runtime.
     abstract fun placeDao(): PlaceDao
 
     companion object {
+        //volatile ensures visibility of INSTANCE across threads
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
+            // if INSTANCE already exists, return it
+            // otherwise, creates it in a thread-safe way
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "places_db"
                 )
+                    // if schema changes and no migration is provided this will wipe and rebuild
+                    // the database
                     .fallbackToDestructiveMigration()
                     .build()
 
+                //returns the newly created instance
                 INSTANCE = instance
                 instance
             }
