4673e2698d887d806846ba675b3c6629000399b1
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:07:49 -0400

    commented mapview.kt

--- app/src/main/java/edu/nd/pmcburne/hello/ui/theme/MapView.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/ui/theme/MapView.kt
@@ -10,17 +10,22 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import edu.nd.pmcburne.hello.data.PlaceEntity
 
+//composable function that displays a map of the places
 @Composable
 fun MapViewScreen(places: List<PlaceEntity>) {
+    //default map center
     val defaultPosition = places.firstOrNull()?.let {
         LatLng(it.latitude, it.longitude)
     } ?: LatLng(38.03474, -78.50820)
 
+    //remembers the camera position state and initializes zoom level
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(defaultPosition, 15f)
     }
 
+    //displays google map
     GoogleMap(cameraPositionState = cameraPositionState) {
+        //adds a marker for each place in the list
         places.forEach { place ->
             Marker(
                 state = MarkerState(LatLng(place.latitude, place.longitude)),
