67937e7b79ca287d48ecb4d206fcae08e3d37be0
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:06:54 -0400

    commented apiservice.kt

--- app/src/main/java/edu/nd/pmcburne/hello/network/ApiService.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/network/ApiService.kt
@@ -4,13 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+//defines API endpoints for network requests using Retrofit
 interface ApiService {
+    //sends a GET request to "placemarks.json"
     @GET("placemarks.json")
     suspend fun getPlacements(): List<PlaceDTO>
 
     companion object {
+        //base URL for all API requests
         private const val BASE_URL = "https://www.cs.virginia.edu/~wxt4gm/"
 
+        //creates and configures a retrofit instance for this API
         fun create(): ApiService {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
