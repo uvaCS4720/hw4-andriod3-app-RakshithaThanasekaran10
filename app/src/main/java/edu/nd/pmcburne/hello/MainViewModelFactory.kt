08c648ab58bbc21717333b32e3a9ae8470cf678f
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-04-03 20:07:36 -0400

    commented mainviewmodelfactory.kt

--- app/src/main/java/edu/nd/pmcburne/hello/MainViewModelFactory.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/MainViewModelFactory.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import edu.nd.pmcburne.hello.data.PlaceDao
 import edu.nd.pmcburne.hello.network.ApiService
 
+//factory class used to create mainviewmodel with dependencies
 class MainViewModelFactory(
     private val apiService: ApiService,
     private val placeDao: PlaceDao
 ) : ViewModelProvider.Factory {
+    // called by the funciton to instantiate teh viewmodel
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         return MainViewModel(apiService, placeDao) as T
